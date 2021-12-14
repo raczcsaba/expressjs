581b216ec97961acac7e51ca5de09077bff724bb
rácz csaba <89366169+mokaesszorakozas@users.noreply.github.com>
2021-12-14 21:43:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // Root endpoint
+
+app.use(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      next();
+    });
+	
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
