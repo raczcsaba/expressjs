4939823f4396acdea81bb073f035619d62533943
rácz csaba <89366169+mokaesszorakozas@users.noreply.github.com>
2021-12-15 14:12:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var db = require("./database.js")
 var md5 = require("md5")
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+var router = express.Router()
 
 // get config vars
 dotenv.config();
@@ -12,24 +13,14 @@ dotenv.config();
 // access config var
 process.env.TOKEN_SECRET;
 
-function generateAccessToken(username) {
+function generateAccessToken(id) {
   return  jwt.sign({
-          username: username
+          userid: id
       },
       process.env.TOKEN_SECRET, {
           expiresIn: "1hr",
           algorithm: 'HS256'
       });
-
-}
-
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if (token == null) return res.sendStatus(401)
-
-    else next();
 
 }
 
@@ -48,11 +39,32 @@ app.use(function (req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      next();
-    });
-	
-app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    if(req.path!='/api/login') {
+        console.log("hmmm1")
+
+        //token authorization
+        const token = req.headers['authorization']
+        if (token == null) {
+            res.status(401).send('Lépj be először');
+            console.log("hmmm2");
+            return;
+
+        }
+        console.log("hmmm3")
+
+        jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+            if (err == null) {
+                //console.log("hmmm4")
+            }else{
+                console.log("hmmm6")
+                res.status(401).send('Érvénytelen token');
+                return;
+                console.log("hmmm7")
+            }
+        });
+        console.log("hmmm5")
+        next();
+    }
 });
 
 // Insert here other API endpoints
@@ -80,9 +92,9 @@ app.post("/api/login",(req, res, next) => {
         if(row.password==req.body.password){
             res.json({
                 "message":"success",
-                "data":generateAccessToken(req.body.name)
+                "data":generateAccessToken(row.id)
             })
-            return;
+            next();
         }else {
             res.status(400).json({"error": "rossz jelszo"});
             return;
@@ -90,22 +102,9 @@ app.post("/api/login",(req, res, next) => {
     });
 });
 
-app.get("/api/users", authenticateToken,(req, res, next) => {
-    var sql = "select * from user"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
+app.get("/api/product",(req, res, next) => {
 
-app.get("/api/product", (req, res, next) => {
+    console.log("hmmm7")
     var sql = "select * from product"
     var params = []
     db.all(sql, params, (err, rows) => {
@@ -120,88 +119,7 @@ app.get("/api/product", (req, res, next) => {
       });
 });
 
-app.get("/api/user/:id", (req, res, next) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-app.post("/api/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : md5(req.body.password)
-    }
-    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-app.patch("/api/user/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : null
-    }
-    db.run(
-        `UPDATE user set 
-           name = COALESCE(?,name), 
-           email = COALESCE(?,email), 
-           password = COALESCE(?,password) 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data,
-                changes: this.changes
-            })
-    });
-})
-app.delete("/api/user/:id", (req, res, next) => {
-    db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", changes: this.changes})
-    });
-})
+
 // Default response for any other request
 app.use(function(req, res){
     res.status(404);
